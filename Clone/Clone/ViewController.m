12da1a42e637997fdbf0e56c88997dcabe978f48
EbichuI <973731246@qq.com>
2016-11-30 10:58:14 +0800

    如果围观无人过问

--- Clone/Clone/ViewController.m
+++ Clone/Clone/ViewController.m
@@ -21,19 +21,19 @@
     self.view.backgroundColor = [UIColor redColor];
     
     UIButton *loginBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-    [loginBtn setTitle:@"登录" forState:UIControlStateNormal];
-    [loginBtn setTitleColor:WHITECOLOR forState:UIControlStateNormal];
-    loginBtn.backgroundColor = REDCOLOR;
-    loginBtn.titleLabel.font = [UIFont systemFontOfSize:20];
-    loginBtn.layer.cornerRadius = 10;
-    [self.view addSubview:loginBtn];
-    [loginBtn makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(pwdTF.bottom).offset(30);
-        make.left.equalTo(self.view.left).offset(WIDTH/2-100);
-        make.width.equalTo(200);
-        make.height.equalTo(40);
-    }];
-    
+//    [loginBtn setTitle:@"登录" forState:UIControlStateNormal];
+//    [loginBtn setTitleColor:WHITECOLOR forState:UIControlStateNormal];
+//    loginBtn.backgroundColor = REDCOLOR;
+//    loginBtn.titleLabel.font = [UIFont systemFontOfSize:20];
+//    loginBtn.layer.cornerRadius = 10;
+//    [self.view addSubview:loginBtn];
+//    [loginBtn makeConstraints:^(MASConstraintMaker *make) {
+//        make.top.equalTo(pwdTF.bottom).offset(30);
+//        make.left.equalTo(self.view.left).offset(WIDTH/2-100);
+//        make.width.equalTo(200);
+//        make.height.equalTo(40);
+//    }];
+//    
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         self.view.hidden = YES;
     });
